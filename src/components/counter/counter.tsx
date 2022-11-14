67414a4c9b7fb998c855c66b886658da30de8502
Mark Grealish <mark@bhalash.com>
2022-11-14 21:39:09 +0000

    Initial commit - add test component

---
+++ src/components/counter/counter.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import styles from './counter.module.scss';
+
+export interface CounterProps {
+  initialCount?: number;
+}
+
+function Counter({ initialCount = 0 }: CounterProps = {}) {
+  const [count, setCount] = useState(initialCount);
+
+  useEffect(() => {
+    console.log(`[Counter] count ${count}`);
+  }, [count]);
+
+  return (
+    <>
+      <h3>{count}</h3>
+      <div>
+        <button onClick={() => setCount(count + 1)}>Add!</button>
+        <button onClick={() => setCount(count - 1)}>Subtract!</button>
+      </div>
+    </>
+  );
+};
+
+export default Counter;
